879a96c10743fc930707e0acf372b4b2efbbab03
AkharanCS <ch21b009@smail.iitm.ac.in>
2025-03-02 00:17:03 +0530

    added code for q1

---
+++ Q1.py
@@ -0,0 +1,26 @@
+import keras
+from keras.datasets import fashion_mnist
+from matplotlib import pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+# Plotting one iamge from each class
+one_image = {}
+c = 0
+for cl,img in zip(train_labels,train_images):
+    if cl not in one_image:
+        one_image[cl] = img
+        c += 1
+    if c == 10:
+        break
+
+
+for cl,img in one_image.items():
+    plt.imshow(img, cmap='gray') 
+    plt.title(f"class: {cl}")
+    plt.axis('off')
+    plt.show()
+
+
+    
